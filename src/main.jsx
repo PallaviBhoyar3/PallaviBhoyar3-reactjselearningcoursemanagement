dafd1e0c7ff9f8200f976543713a9bcf014031a9
pallavibhoyar <pallavi.prakshrao-bhoyar@capgemini.com>
2025-05-05 11:42:43 +0530

    create components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import App from './App';
+import { SidebarProvider } from './context/sidebar_context';
+import { CoursesProvider } from './context/courses_context';
+import { CartProvider } from './context/cart_context';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  <SidebarProvider>
+    <CoursesProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </CoursesProvider>
+  </SidebarProvider>
 )
